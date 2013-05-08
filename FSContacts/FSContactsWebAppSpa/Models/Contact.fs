13d82fc604a28c2fa4542ef8c0d8b5f2a82ed70c
Richard Broida <SirEel@Outlook.com>
2013-05-08 06:12:24 -0400

    again

--- FSContacts/FSContactsWebAppSpa/Models/Contact.fs
+++ FSContacts/FSContactsWebAppSpa/Models/Contact.fs
@@ -7,3 +7,6 @@ type Contact() =
     member x.FirstName with get() = firstName and set v = firstName <- v
     member x.LastName with get() = lastName and set v = lastName <- v
     member x.Phone with get() = phone and set v = phone <- v
+
+
+
