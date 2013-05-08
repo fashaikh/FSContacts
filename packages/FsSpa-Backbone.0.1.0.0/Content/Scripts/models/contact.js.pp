f410cf3b4016e57e628583ea156e1e9a6b0028d5
Your full name <faizmsdn@live.com>
2013-05-08 15:12:39 -0700

    deleting packages folder

--- packages/FsSpa-Backbone.0.1.0.0/Content/Scripts/models/contact.js.pp
+++
@@ -1,8 +0,0 @@
-﻿$(function () {
-    appFsMvc.Contact = Backbone.Model.extend();
-
-    appFsMvc.ContactCollection = Backbone.Collection.extend({
-        model: window.appFsMvc.Contact,
-        url: "/api/contacts/"
-    });
-});
